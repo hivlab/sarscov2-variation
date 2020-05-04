6774ad9354a69dbdf8cc03d04fbea444bcd4521c
Taavi Päll <tapa741@gmail.com>
2020-05-04 15:58:11 +0300

    simpler wrapper

--- wrappers/sequences/rename_fasta/wrapper.py
+++ wrappers/sequences/rename_fasta/wrapper.py
@@ -1,16 +1,13 @@
-from Bio import SeqIO
+from snakemake.shell import shell
 from hashlib import blake2s
 
 sample = snakemake.params.get("sample", "")
 stub = snakemake.params.get("stub", "")
 assert len(sample) > 0, "Sample name is missing"
 h = blake2s()
+h.update(sample.encode("utf-8"))
+new_id = stub + h.hexdigest()
 
-with open(snakemake.input[0], "r") as input_handle, open(
-    snakemake.output[0], "w"
-) as output_handle:
-    for record in SeqIO.parse(input_handle, "fasta"):
-        h.update(sample.encode("utf-8"))
-        record.id = h.hexdigest()
-        record.description = stub + h.hexdigest()
-        SeqIO.write(record, output_handle, "fasta")
+shell(
+    "sed 's/^>.*$/>{new_id}/g' {snakemake.input[0]} > {snakemake.output[0]}"
+    )
