2624641717ea08d6cdd656fb5a9baba09d8af744
Taavi Päll <tapa741@gmail.com>
2020-05-04 15:44:50 +0300

    fix snakemake io

--- wrappers/sequences/rename_fasta/wrapper.py
+++ wrappers/sequences/rename_fasta/wrapper.py
@@ -7,7 +7,7 @@ assert len(sample) > 0, "Sample name is missing"
 h = blake2s()
 
 with open(snakemake.input[0], "r") as input_handle, open(
-    snakemake.output, "w"
+    snakemake.output[0], "w"
 ) as output_handle:
     for record in SeqIO.parse(input_handle, "fasta"):
         h.update(sample.encode("utf-8"))
